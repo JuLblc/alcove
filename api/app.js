c3883ba94b0b5e7c505cb91b94be67e8b2db0641
JuLblc <julien.leblanc@epfedu.fr>
2023-01-02 21:47:48 +0100

    cleaning dependency

--- api/app.js
+++ api/app.js
@@ -4,7 +4,6 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const favicon = require("serve-favicon");
-const hbs = require("hbs");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
@@ -35,11 +34,6 @@ mongoose
   .catch((err) => {
     console.error("Error connecting to mongo", err);
   });
-
-const app_name = require("../package.json").name;
-const debug = require("debug")(
-  `${app_name}:${path.basename(__filename).split(".")[0]}`
-);
 
 const app = express();
 
@@ -116,7 +110,7 @@ app.locals.title = "Express - Generated with IronGenerator";
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000"],
+    origin: [process.env.NOM_DOMAINE],
   })
 );
 
